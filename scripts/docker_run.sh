776d11542e168203c270a5b34a9c413ac2dc4f7d
QiYao-Wang <wangqiyao25@mails.ucas.ac.cn>
2026-03-30 11:05:25 +0800

    docker_run

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -15,6 +15,10 @@ PROJECT_PATH="/data/shared/users/wangqiyao/Interactweb-Bench"
 docker run -it --rm \
   --name interactweb_test-2 \
   --network host \
-  -v "${PROJECT_PATH}:/app" \
+  -v "${PROJECT_PATH}/src:/app/src" \
+  -v "${PROJECT_PATH}/data:/app/data" \
+  -v "${PROJECT_PATH}/experiment_results:/app/experiment_results" \
+  -v "${PROJECT_PATH}/.env:/app/.env" \
+  -v "${PROJECT_PATH}/config.yaml:/app/config.yaml" \
   --entrypoint /bin/bash \
   interactweb-bench:v1.0
