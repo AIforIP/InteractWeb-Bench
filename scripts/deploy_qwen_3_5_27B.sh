6ef3d4f58d06c6f516f5e0fe1511816ec123a814
QiYao-Wang <wangqiyao25@mails.ucas.ac.cn>
2026-03-30 14:46:45 +0800

    qwen3.5-27b

--- scripts/deploy_qwen_3_5_27B.sh
+++ scripts/deploy_qwen_3_5_27B.sh
@@ -14,11 +14,11 @@ echo "Starting vLLM Node 1 on port: $TARGET_PORT"
 
 fuser -k ${TARGET_PORT}/tcp >/dev/null 2>&1
 
-MODEL_PATH="/data/shared/users/wangqiyao/models/Qwen3-Coder-30B-A3B-Instruct"
+MODEL_PATH="/data/shared/users/wangqiyao/models/Qwen3.5-27B"
 
 CUDA_VISIBLE_DEVICES=3,4 python -m vllm.entrypoints.openai.api_server \
     --model $MODEL_PATH \
-    --served-model-name "Qwen3-Coder-30B-A3B-Instruct" \
+    --served-model-name "Qwen3.5-27B" \
     --port $TARGET_PORT \
     --host 0.0.0.0 \
     --trust-remote-code \
